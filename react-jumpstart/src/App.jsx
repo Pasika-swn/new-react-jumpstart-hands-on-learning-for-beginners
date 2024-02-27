42de84f44391f1f8cac0983ae567bfbd72d4efae
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-27 20:39:49 +0700

    default parameter

--- react-jumpstart/src/App.jsx
+++ react-jumpstart/src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 // props is immutable obj
-const SelfIntroduction = (props) => {
-  return <h1>"My name is {props.name}."</h1>;
+const SelfIntroduction = ({ name = 'unknown' }) => {
+  return <h1>"My name is {name}."</h1>;
 };
 
 function App() {
@@ -13,6 +13,10 @@ function App() {
     <div className="App">
       {/* ขึ้นด้วยตัวใหญ่ คิดว่าเป็น component เสมอ */}
       <SelfIntroduction name="Beam" />
+      <SelfIntroduction name="" />
+
+      <SelfIntroduction name={undefined} />
+      <SelfIntroduction />
     </div>
   );
 }
