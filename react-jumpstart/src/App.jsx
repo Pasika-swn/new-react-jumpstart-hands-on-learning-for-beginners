bb303691af68d45baba9393ef63ee45c6efc75fe
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-27 20:45:53 +0700

    conditional rendering

--- react-jumpstart/src/App.jsx
+++ react-jumpstart/src/App.jsx
@@ -4,15 +4,24 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 // props is immutable obj
-const SelfIntroduction = ({ name = 'unknown' }) => {
-  return <h1>"My name is {name}."</h1>;
+const SelfIntroduction = ({ name = "unknown", DOB }) => {
+  return (
+    // we can return just only one jsx elem
+    <div>
+      <h1>"My name is {name}."</h1>;
+
+      {/* conditional rendering => shorted-if */}
+      {DOB ? <h2>I was born in {DOB}</h2> : null}
+      
+    </div>
+  );
 };
 
 function App() {
   return (
     <div className="App">
       {/* ขึ้นด้วยตัวใหญ่ คิดว่าเป็น component เสมอ */}
-      <SelfIntroduction name="Beam" />
+      <SelfIntroduction name="Beam" DOB="22/9/37" />
       <SelfIntroduction name="" />
 
       <SelfIntroduction name={undefined} />
