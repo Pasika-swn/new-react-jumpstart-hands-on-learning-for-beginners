782fadce2532b31e28f1dfd271e18fe9e5a32fb5
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-27 20:55:12 +0700

    render array

--- react-jumpstart/src/App.jsx
+++ react-jumpstart/src/App.jsx
@@ -4,15 +4,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 // props is immutable obj
-const SelfIntroduction = ({ name = "unknown", DOB }) => {
+const SelfIntroduction = ({ name = "unknown", DOB, hobbies=[] }) => {
   return (
     // we can return just only one jsx elem
     <div>
-      <h1>"My name is {name}."</h1>;
-
-      {/* conditional rendering => shorted-if */}
+      <h1>"My name is {name}."</h1>;{/* conditional rendering => shorted-if */}
       {DOB ? <h2>I was born in {DOB}</h2> : null}
-      
+      {hobbies.length > 0 ? <h2>My hobbies are "{hobbies.join(", ")}"</h2> : null}
     </div>
   );
 };
@@ -21,11 +19,13 @@ function App() {
   return (
     <div className="App">
       {/* ขึ้นด้วยตัวใหญ่ คิดว่าเป็น component เสมอ */}
-      <SelfIntroduction name="Beam" DOB="22/9/37" />
-      <SelfIntroduction name="" />
+      <SelfIntroduction
+        name="Beam"
+        DOB="22/9/37"
 
-      <SelfIntroduction name={undefined} />
-      <SelfIntroduction />
+        // dont wanna render hobbies
+        // hobbies={["playing game", "watching movies", "Eat"]}
+      />
     </div>
   );
 }
